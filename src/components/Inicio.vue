3127ac7e6ea582048df450c28dbc9375608d13ab
Edson733 <20213tn034@utez.edu.mx>
2024-02-01 12:16:15 -0600

    Estructura para el examen

---
+++ src/components/Inicio.vue
@@ -0,0 +1,9 @@
+<template>
+    <b-container>
+        <h1>Inicio</h1>
+    </b-container>
+</template>
+  
+<script setup></script>
+  
+<style scoped></style>
---
+++ src/router/router.js
@@ -0,0 +1,33 @@
+import VueRouter from "vue-router";
+import Vue from "vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+    //la ruta base
+    {
+        path: '/',
+        redirect: '/inicio'
+    },
+    {
+        path: '/',
+        component: {
+            render(c) {
+                return c('router-view');
+            },
+        },
+        children: [
+            {
+                path: '/inicio',
+				name: 'inicio',
+				component: () => import('../components/Inicio.vue')
+            },
+        ]
+    }
+]
+
+const router = new VueRouter({
+    mode: 'history', // Use 'history' mode to remove the hash from the URL
+    routes
+})
+export default router;
